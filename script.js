25853888157e99b3d91018c8ce45511b1e367b4b
CodeSliceWebSolutions <96576667+CodeSliceWebSolutions@users.noreply.github.com>
2026-01-05 14:13:52 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,49 @@
-let readCount = 0;
+import { doc, getDoc, setDoc } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
+import { auth, db } from "./firebase.js";
 
-const button = document.getElementById("readBtn");
-const counter = document.getElementById("counter");
+import { collection, getDocs } from
+"https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
 
-button.addEventListener("click", () => {
-	readCount++;
-	counter.textContent = `Times read: ${readCount}`;
+
+const snap = await getDocs(collection(db, "users"));
+
+snap.forEach(doc => {
+  console.log(doc.id, doc.data().progress);
+});
+
+
+const ADMIN_UID = "zPuzBifNNAgtRGq1MMth6sUl4SC2";
+
+
+let count = 0;
+const countEl = document.getElementById("count");
+
+auth.onAuthStateChanged(async user => {
+  if (!user) return;
+
+  const ref = doc(db, "users", user.uid);
+  const snap = await getDoc(ref);
+
+  if (snap.exists()) {
+    count = snap.data().progress?.lesson1 || 0;
+  } else {
+    await setDoc(ref, { progress: { lesson1: 0 } });
+    count = 0;
+  }
+
+  countEl.textContent = count;
+});
+
+document.getElementById("readBtn").addEventListener("click", async () => {
+  const user = auth.currentUser;
+  if (!user) return;
+
+  count++;
+  countEl.textContent = count;
+
+  await setDoc(
+    doc(db, "users", user.uid),
+    { progress: { lesson1: count } },
+    { merge: true }
+  );
 });
