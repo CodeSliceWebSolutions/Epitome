fdf3f2149aea3b739d8aa06b8f17dc4896554396
CodeSliceWebSolutions <96576667+CodeSliceWebSolutions@users.noreply.github.com>
2026-01-09 13:35:32 +0200

    Update script.js

--- script.js
+++ script.js
@@ -2,8 +2,34 @@ import { doc, getDoc, setDoc } from
 "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
 import { auth, db } from "./firebase.js";
 
+const lessonMessages = {
+  0: "message 1.",
+  1: "Gmessage 2.",
+  2: "N3.",
+  3: "St4.",
+  4: "Lemessage 5."
+};
+
 let count = 0;
+
 const countEl = document.getElementById("count");
+const messageEl = document.getElementById("lessonMessage");
+const lessonContentEl = document.getElementById("lessonContent");
+const readBtn = document.getElementById("readBtn");
+
+function renderLesson(count) {
+  if (count >= 4) {
+    messageEl.textContent = lessonMessages[4];
+    readBtn.disabled = true;
+
+    lessonContentEl.innerHTML = `
+      <h1>progress</h1>
+      
+    `;
+  } else {
+    messageEl.textContent = lessonMessages[count];
+  }
+}
 
 auth.onAuthStateChanged(async user => {
   if (!user) return;
@@ -19,14 +45,16 @@ auth.onAuthStateChanged(async user => {
   }
 
   countEl.textContent = count;
+  renderLesson(count);
 });
 
-document.getElementById("readBtn").addEventListener("click", async () => {
+readBtn.addEventListener("click", async () => {
   const user = auth.currentUser;
   if (!user) return;
 
   count++;
   countEl.textContent = count;
+  renderLesson(count);
 
   await setDoc(
     doc(db, "users", user.uid),
